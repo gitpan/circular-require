b2f68afc35a885ea6b491e3f05acb6fa492d8258
Jesse Luehrs <doy@cpan.org>
2011-01-25 21:36:47 +0000

    Import of DOY/circular-require-0.01 from CPAN.
    gitpan-cpan-distribution: circular-require
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         DOY/circular-require-0.01.tar.gz
    gitpan-cpan-author:       DOY
    gitpan-cpan-maturity:     released

---
+++ lib/circular/require.pm
@@ -0,0 +1,144 @@
+package circular::require;
+BEGIN {
+  $circular::require::VERSION = '0.01';
+}
+use strict;
+use warnings;
+# ABSTRACT: detect circularity in use/require statements
+
+use Package::Stash;
+
+
+my %seen;
+my $saved;
+
+sub _require {
+    my ($file) = @_;
+    if (exists $seen{$file} && !$seen{$file}) {
+        warn "Circular require detected: $file (from " . caller() . ")\n";
+    }
+    $seen{$file} = 0;
+    my $ret = $saved ? $saved->($file) : CORE::require($file);
+    $seen{$file} = 1;
+    return $ret;
+}
+
+sub import {
+    my $stash = Package::Stash->new('CORE::GLOBAL');
+    if ($saved) {
+        $stash->add_package_symbol('&require' => $saved);
+    }
+    else {
+        $stash->remove_package_symbol('&require');
+    }
+}
+
+sub unimport {
+    my $stash = Package::Stash->new('CORE::GLOBAL');
+    my $old_require = $stash->get_package_symbol('&require');
+    $saved = $old_require
+        if defined($old_require) && $old_require != \&_require;
+    $stash->add_package_symbol('&require', \&_require);
+}
+
+
+1;
+
+__END__
+=pod
+
+=head1 NAME
+
+circular::require - detect circularity in use/require statements
+
+=head1 VERSION
+
+version 0.01
+
+=head1 SYNOPSIS
+
+  package Foo;
+  use Bar;
+
+  package Bar;
+  use Foo;
+
+  package main;
+  no circular::require;
+  use Foo; # warns
+
+or
+
+  perl -M-circular::require foo.pl
+
+=head1 DESCRIPTION
+
+Perl by default just ignores cycles in require statements - if Foo.pm does
+C<use Bar> and Bar.pm does C<use Foo>, doing C<use Foo> elsewhere will start
+loading Foo.pm, then hit the C<use> statement, start loading Bar.pm, hit the
+C<use> statement, notice that Foo.pm has already started loading and ignore it,
+and continue loading Bar.pm. But Foo.pm hasn't finished loading yet, so if
+Bar.pm uses anything from Foo.pm (which it likely does, if it's loading it),
+those won't be accessible while the body of Bar.pm is being executed. This can
+lead to some very confusing errors, especially if introspection is happening at
+load time (C<make_immutable> in L<Moose> classes, for example). This module
+generates a warning whenever a module is skipped due to being loaded, if that
+module has not finished executing.
+
+=head1 CAVEATS
+
+This module works by overriding C<CORE::GLOBAL::require>, and so other modules
+which do this may cause issues if they aren't written properly. This also means
+that the effect is global, but this is typically the most useful usage.
+
+=for Pod::Coverage unimport
+
+=head1 BUGS
+
+No known bugs.
+
+Please report any bugs through RT: email
+C<bug-circular-require at rt.cpan.org>, or browse to
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=circular-require>.
+
+=head1 SUPPORT
+
+You can find this documentation for this module with the perldoc command.
+
+    perldoc circular::require
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/circular-require>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/circular-require>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=circular-require>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/circular-require>
+
+=back
+
+=head1 AUTHOR
+
+Jesse Luehrs <doy at tozt dot net>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Jesse Luehrs.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
---
+++ t/01-basic.t
@@ -0,0 +1,70 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use lib 't/01';
+use Test::More;
+
+use circular::require ();
+
+circular::require->unimport;
+
+{
+    my $warnings;
+    local $SIG{__WARN__} = sub { $warnings .= $_[0] };
+    use_ok('Foo');
+    is($warnings, "Circular require detected: Foo.pm (from Baz)\nCircular require detected: Baz.pm (from Bar)\n", "correct warnings");
+    clear();
+}
+
+{
+    my $warnings;
+    local $SIG{__WARN__} = sub { $warnings .= $_[0] };
+    use_ok('Bar');
+    is($warnings, "Circular require detected: Baz.pm (from Foo)\nCircular require detected: Bar.pm (from Baz)\n", "correct warnings");
+    clear();
+}
+
+{
+    my $warnings;
+    local $SIG{__WARN__} = sub { $warnings .= $_[0] };
+    use_ok('Baz');
+    is($warnings, "Circular require detected: Baz.pm (from Foo)\n", "correct warnings");
+    clear();
+}
+
+circular::require->import;
+
+{
+    my $warnings;
+    local $SIG{__WARN__} = sub { $warnings .= $_[0] };
+    use_ok('Foo');
+    is($warnings, undef, "correct warnings");
+    clear();
+}
+
+{
+    my $warnings;
+    local $SIG{__WARN__} = sub { $warnings .= $_[0] };
+    use_ok('Bar');
+    is($warnings, undef, "correct warnings");
+    clear();
+}
+
+{
+    my $warnings;
+    local $SIG{__WARN__} = sub { $warnings .= $_[0] };
+    use_ok('Baz');
+    is($warnings, undef, "correct warnings");
+    clear();
+}
+
+sub clear {
+    for (qw(Foo Bar Baz)) {
+        no strict 'refs';
+        delete $::{$_};
+        delete ${$_ . '::'}{quux};
+        delete $INC{"$_.pm"};
+    }
+}
+
+done_testing;
